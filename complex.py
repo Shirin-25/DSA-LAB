7c7c934d08c1682f179226eaf72ed0d76eda1f15
Shirin-25 <shirinthabasum@gmail.com>
2025-08-01 14:35:01 +0530

    Add files via upload

---
+++ complex.py
@@ -0,0 +1,17 @@
+import time
+
+# Take three inputs from the user
+n1 = int(input("Enter first value of n: "))
+n2 = int(input("Enter second value of n: "))
+n3 = int(input("Enter third value of n: "))
+
+for n in [n1, n2, n3]:
+    start = time.time()
+
+    total = 0
+    for i in range(n):
+        total += i  # simple operation
+
+    end = time.time()
+
+    print(f"Time taken for n = {n} is {end - start:.6f} seconds")
